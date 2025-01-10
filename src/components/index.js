97f592cdc9df6325d22329092a476177b3ce0a44
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-01-10 11:20:47 +0300

    components and props destructuring

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+import Hero from "./Hero";
+import HomeCards from "./HomeCards";
+import Card from "./Card";
+
+
+
+
+export {
+    Nav,
+    Hero,
+    HomeCards,
+    Card,
+    };
